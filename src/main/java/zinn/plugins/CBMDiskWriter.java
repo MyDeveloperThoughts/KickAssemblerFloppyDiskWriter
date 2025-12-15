6d5ff49845f2634d8250bd4dbefc0dd64a8df36a
Chris Zinn <czinn@forge-tech.com>
2025-12-14 19:19:53 -0500

    Create the byte[] memory blocks

--- src/main/java/zinn/plugins/CBMDiskWriter.java
+++ src/main/java/zinn/plugins/CBMDiskWriter.java
@@ -2,10 +2,14 @@ package zinn.plugins;
 
 import kickass.plugins.interf.diskwriter.DiskWriterDefinition;
 import kickass.plugins.interf.diskwriter.IDiskData;
+import kickass.plugins.interf.diskwriter.IDiskFileData;
 import kickass.plugins.interf.diskwriter.IDiskWriter;
 import kickass.plugins.interf.general.IEngine;
+import kickass.plugins.interf.general.IMemoryBlock;
+import kickass.plugins.interf.general.IParameterMap;
 
 import java.io.OutputStream;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
 
@@ -48,6 +52,38 @@ public final class CBMDiskWriter implements IDiskWriter
 
         AbstractDisk disk = AbstractDisk.createDisk(fileName, name, id, driveType);
         disk.formatDisk();
+
+        List<IDiskFileData> filesFromAssembler = diskData.getFiles();
+        for(IDiskFileData file : filesFromAssembler)
+        {
+            IParameterMap fileParams = file.getParameters();
+            List<IMemoryBlock> memoryBlocks = file.getMemoryBlocks();
+
+            // Unpack the memory blocks into C64 Memory
+            byte[] c64RAM = new byte[65535];
+            for(IMemoryBlock memoryBlock : memoryBlocks)
+            {
+                byte[] memoryBlockMemory = memoryBlock.getBytes();
+                ByteLogic.copyIntoRawBytes(c64RAM, memoryBlockMemory, memoryBlock.getStartAddress());
+            }
+
+            // Slice out the part we want to write to disk
+            int lowestMemoryAddress = memoryBlocks.stream().mapToInt(IMemoryBlock::getStartAddress).min().orElse(0);
+            int highestMemoryAddress = memoryBlocks.stream().mapToInt(i -> i.getStartAddress() + i.getBytes().length).max().orElse(0);
+            byte[] binaryToWrite = Arrays.copyOfRange(c64RAM, lowestMemoryAddress, highestMemoryAddress);
+
+            boolean storeStartAddress = true;
+            if (storeStartAddress)
+            {
+                byte[] temp = new byte[binaryToWrite.length + 2];
+                ByteLogic.copyIntoRawBytes(temp, binaryToWrite, 2);
+                temp[0] = (byte) (lowestMemoryAddress & 256);
+                temp[1] = (byte) (lowestMemoryAddress / 256);
+                binaryToWrite = temp;
+            }
+
+            // todo: Store the binary file in the filesystem
+        }
 
         try(OutputStream output = engine.openOutputStream(fileName))
         {
