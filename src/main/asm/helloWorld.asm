97a93640983af95f40f8437a1a5cf8507b62f951
Chris Zinn <czinn@forge-tech.com>
2025-12-18 10:39:32 -0500

    Better 1571 test disk

--- src/main/asm/helloWorld.asm
+++ src/main/asm/helloWorld.asm
@@ -38,8 +38,8 @@ go:
 .disk multidisk [filename="test.d71", name="STUFF", id="CZ", driveType="1571"]
 {
         [name="HELLO WORLD", type="prg", segments = "CODE"],
-        [name="SID PLAYER", type="prg", prgFiles="testfiles/sidplayer.64"],
-        [name="SID.OBJ.64", type="prg", prgFiles="testfiles/sid.obj.64"],
+        [name="SID PLAYER 128", type="prg", prgFiles="testfiles/sidplayer.128"],
+        [name="SID.OBJ.128", type="prg", prgFiles="testfiles/tsonata6-3.mus"],
         [name="SID BURNER.MUS", type="prg", prgFiles="testfiles/sidburner.mus"],
 }
 
